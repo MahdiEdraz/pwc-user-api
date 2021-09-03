a55ab9a7b8d49aeb0bf3830899ed263d8c75be02
mahdiedraz <you@example.com>
2021-09-03 20:19:03 +0300

    create new dataAccess with crud operations for projects

---
+++ src/main/java/com/pwc/users/dataacess/ProjectDataAccess.java
@@ -0,0 +1,97 @@
+package com.pwc.users.dataacess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pwc.users.dao.Project;
+import com.pwc.users.dao.User;
+
+public class ProjectDataAccess {
+	public List<Project> getProjectsDetails(){
+        String query = "select * from project";
+    	
+		Connection conn =MySqlConnectionPool.getConnectoin();
+		List<Project> list = new ArrayList();
+		try(PreparedStatement ps = conn.prepareStatement(query)){
+			ResultSet rs=ps.executeQuery();  
+    		while(rs.next()){  
+    			
+    			Project item = new Project();
+    			item.setName(rs.getString("Project_name"));
+    			item.setAssignedUserId(rs.getInt("assigned_userid"));
+    			item.setId(rs.getInt("id"));
+    			list.add(item);
+    		}  
+			MySqlConnectionPool.releaseConnection(conn);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list ;
+	}
+	
+	public List<String> getAllProjects(){
+		 String query = "select * from project_lookup";
+	    	
+			Connection conn =MySqlConnectionPool.getConnectoin();
+			List<String> list = new ArrayList();
+			try(PreparedStatement ps = conn.prepareStatement(query)){
+				ResultSet rs=ps.executeQuery();  
+	    		while(rs.next()){  
+	    			
+	    		
+	    			list.add(rs.getString(2));
+	    		}  
+				MySqlConnectionPool.releaseConnection(conn);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			return list ;
+	}
+	public void updateUserProject(Project project,String perviousProject) {
+		String query = " update project set project_name = ? , assigned_userid = ?  where project_name = ? and assigned_userid = ?" ;
+		Connection conn =MySqlConnectionPool.getConnectoin();
+		try(PreparedStatement ps = conn.prepareStatement(query)){
+            ps.setString(1, project.getName());  
+            ps.setInt(2, project.getAssignedUserId());
+            ps.setString(3, perviousProject);
+            ps.setInt(4, project.getAssignedUserId());
+
+            ps.executeUpdate();
+  			MySqlConnectionPool.releaseConnection(conn);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void deleteUserFromProject(Project project) {
+		String query = " delete from project where project_Name = ? and assigned_userid = ?" ;
+		Connection conn =MySqlConnectionPool.getConnectoin();
+		try(PreparedStatement ps = conn.prepareStatement(query)){
+            ps.setString(1, project.getName());  
+            ps.setInt(2, project.getAssignedUserId());  
+            ps.executeUpdate();
+  			MySqlConnectionPool.releaseConnection(conn);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public static void main(String args[]) {
+		ProjectDataAccess da = new ProjectDataAccess();
+		Project p = new Project();
+		p.setName("Dev");
+		p.setAssignedUserId(2);
+		da.updateUserProject(p, "BlockChain");
+		System.out.println();
+	}
+}
