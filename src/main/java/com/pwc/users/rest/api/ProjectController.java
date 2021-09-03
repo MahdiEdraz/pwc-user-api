bbd7a92111c25898e4d4903a857c5c4fd41b2f5a
mahdiedraz <you@example.com>
2021-09-04 01:43:44 +0300

    project crud service

---
+++ src/main/java/com/pwc/users/rest/api/ProjectController.java
@@ -0,0 +1,70 @@
+package com.pwc.users.rest.api;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.pwc.users.dao.Department;
+import com.pwc.users.dao.Project;
+import com.pwc.users.dataacess.ProjectDataAccess;
+
+@Path("/project")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProjectController {
+    private ProjectDataAccess projectDataAccess = new ProjectDataAccess(); 
+	private GsonBuilder builder = new GsonBuilder(); 
+    private Gson gson = builder.create() ;
+	
+	  @GET
+	    @Path("/getAllProjects")
+	    public Response getAllProjects() {
+	        List<String> list = projectDataAccess.getAllProjects();
+	        String json = gson.toJson(list);
+	        return Response.status(200).entity(json).build();
+	    } 
+	    @GET
+	    @Path("/getProjectsDetails")
+	    public Response getProjectsDetails() {
+	        List<Project> list = projectDataAccess.getProjectsDetails();
+	        String json = gson.toJson(list);
+	        return Response.status(200).entity(json).build();
+	    } 
+	    @POST
+	    @Path("/assignUserToProject/{project}")
+	    public Response assignUserToProject(@PathParam("project") String json) {
+	    	Project project = gson.fromJson(json, Project.class);
+	    	int result = projectDataAccess.addUserToProject(project);
+	    	String res = result == -1 ? "Not Added" : "Added" ;
+	        return Response.status(200).entity(res).build();
+	    }
+	    
+	    @PUT
+	    @Path("/updateUserProject/{project}/{previousProject}")
+	    public Response updateUserDepartment(@PathParam("project") String json,@PathParam("previousProject") String previousProject) {
+	    	Project project = gson.fromJson(json, Project.class);
+	    	int result = projectDataAccess.updateUserProject(project, previousProject);
+	    	String res = result == -1 ? "Not updated" : "Updated" ;
+	    	return Response.status(200).entity(res).build();
+	    }
+	    
+	    @DELETE
+	    @Path("/deleteUserOnProject/{project}")
+	    public Response deleteUserOnProject(@PathParam("project") String json) {
+	    	Project project = gson.fromJson(json, Project.class);
+	    	int result = projectDataAccess.deleteUserFromProject(project);
+	    	String res = result == -1 ? "Not Deleted" : "Deleted" ;
+	        return Response.status(200).entity(res).build();
+	    }
+}
